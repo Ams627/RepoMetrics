d8f3c8a9874cdcff27c2682a932646573c8e1579
Adrian Sims <adriansims62781@gmail.com>
2018-10-09 19:57:30 +0200

    Add project files.

---
+++ RepoMetrics/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepoMetrics
+{
+    internal class Program
+    {
+        /// <summary>
+        /// if true scan the .git folder:
+        /// </summary>
+        static bool git = false;
+
+        /// <summary>
+        /// if true, scan the .VS folder
+        /// </summary>
+        static bool vs = false;
+        private static void Main(string[] args)
+        {
+            try
+            {
+                if (!args.Any())
+                {
+                    throw new Exception("You must supply at least one argument.");
+                }
+
+                foreach (var arg in args)
+                {
+                    if (arg[0] == '-')
+                    {
+                        if (arg.Length == 1)
+                        {
+                            throw new Exception("minus on its own is not permitted as an option.");
+                        }
+                        if (arg[1] == 'g')
+                        {
+                            git = true;
+                        }
+                        else if (arg[1] == '')
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add RepoMetrics/Program.cs
+git add RepoMetrics/App.config
+git add RepoMetrics/Properties/AssemblyInfo.cs
+git add RepoMetrics/RepoMetrics.csproj
+git add RepoMetrics.sln
+git add .gitignore
